6760d3d2052ec3d97b262a67ad12b947ab03a941
Dharni <dharnisingh@gmail.com>
2020-05-12 16:09:43 +0530

    Commit Thread Agrument ile

---
+++ CPP/Threading/Thread_argument.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void callme(int *p)
+{
+	cout << "Inside thread: " << this_thread::get_id() << endl;
+	*p=*p+1;
+}
+
+void refcall(const int &rf)
+{
+	cout << "Inside thread: " << this_thread::get_id() << endl;
+
+}
+
+int main()
+{
+int val = 10;
+int ref_val =10;
+
+thread t1(callme, &val);
+thread t2(refcall, ref_val);
+
+if(t1.joinable())
+	t1.join();
+
+if(t2.joinable())
+	t2.join();
+// Val will be updated by thread function callme.
+cout << "Value after thread execution: " << val << endl;
+cout << "Value after thread execution: " << ref_val << endl;
+
+return 0;
+}
